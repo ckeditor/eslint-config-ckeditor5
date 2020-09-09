0858993caee2164aa342adfc8d106699bb065eec
Kamil Piechaczek <pomek@users.noreply.github.com>
2020-09-09 13:38:35 +0200

    Internal: Fixed a changelog generator script.

--- scripts/changelog.js
+++ scripts/changelog.js
@@ -13,4 +13,7 @@
  * Scripts for generating the changelog before starting the release process.
  */
 
-require( '@ckeditor/ckeditor5-dev-env' ).generateChangelogForSinglePackage();
+require( '@ckeditor/ckeditor5-dev-env' ).generateChangelogForSinglePackage({
+	// TODO: Remove this line after the next release.
+	from: 'e0a02ed4ec4741f2f9015571a88d3ef9030dac28'
+});
